52ec51e68cc3bb8eea9b88d7eea0bc38944e06ae
NatanFonseca <ntn2005@gmail.com>
2020-03-25 00:02:16 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const SessionController = require('./controllers/SessionController');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+/**Listar todas as ONGs */
+routes.get('/ongs', OngController.index);
+/**Cadastrar ONGs no Database */
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
